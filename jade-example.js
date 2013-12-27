903c71f69bea811bf51a79d246c53e90f2f88d54
Azat Mardanov <azat.mardanov@gmail.com>
2013-12-27 13:58:02 -0800

    refactor standalone jade example

--- jade-example.js
+++ jade-example.js
@@ -1,43 +1,31 @@
 var jade = require('jade'),
   fs = require('fs');
 
-fs.readFile('jade-example.jade', 'utf-8', function(error, template){
-  var jadeSample = jade.compile(template);
-  var data = {
-    title: "Practical Node.js",
-    author: "@azat_co",
-    tags: ['express', 'node', 'javascript']
-  }
-  data.body = process.argv[2];
-  var html = jadeSample(data)
+var data = {
+  title: "Practical Node.js",
+  author: {
+    twitter: "@azat_co",
+    name: "Azat"
+  },
+  tags: ['express', 'node', 'javascript']
+}
+data.body = process.argv[2];
+
+fs.readFile('jade-example.jade', 'utf-8', function(error, source){
+  var template = jade.compile(source);
+  var html = template(data)
   console.log(html)
 });
 
 //jade.render
-// var jade = require('jade'),
-//   fs = require('fs');
 
-// fs.readFile('jade-example.jade', 'utf-8', function(error, template){
-//   var data = {
-//     title: "Practical Node.js",
-//     author: "@azat_co",
-//     tags: ['express', 'node', 'javascript']
-//   }
-//   data.body = process.argv[2];
-//   var html = jade.render(template, data)
+// fs.readFile('jade-example.jade', 'utf-8', function(error, source){
+//   var html = jade.render(source, data)
 //   console.log(html)
 // });
 
 //jade.renderFile
 
-// var jade = require('jade'),
-//   fs = require('fs'),
-//   data = {
-//     title: "Practical Node.js",
-//     author: "@azat_co",
-//     tags: ['express', 'node', 'javascript']
-//   };
-// data.body = process.argv[2];
 // jade.renderFile('jade-example.jade', data, function(error, html){
 //   console.log(html)
 // });
