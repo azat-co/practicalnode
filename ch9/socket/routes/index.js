e7f223aa2a9f07c72a3f103024d57ed8abef3539
Azat Mardanov <azat.mardanov@gmail.com>
2014-05-09 20:40:36 -0700

    upgrade and slimdown socket+express (v4)

--- ch9/socket/routes/index.js
+++ ch9/socket/routes/index.js
@@ -1,8 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+/* GET home page. */
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
-};
+});
+
+module.exports = router;
--- ch9/socket3/app.js
+++ ch9/socket3/app.js
@@ -1,23 +1,34 @@
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
@@ -29,15 +40,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
-app.set('port', process.env.PORT || 3000);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
