44a325b743ab9bd72a1f6fd2899af6e2c982ff3c
Azat Mardanov <azat.mardanov@gmail.com>
2014-05-21 11:10:33 -0700

    express 4 and other fixes :sunflower:

--- ch6/password/app.js
+++ ch6/password/app.js
@@ -10,39 +10,47 @@ var express = require('express'),
     users: db.collection('users')
   };
 
+// Express.js Middleware
+var session = require('express-session'),
+  logger = require('morgan'),
+  errorHandler = require('errorhandler'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
 
 var app = express();
-app.locals.appTitle = "blog-express";
+app.locals.appTitle = 'blog-express';
 
+// Expose collections to request handlers
 app.use(function(req, res, next) {
-  if (!collections.articles || ! collections.users) return next(new Error("No collections."))
+  if (!collections.articles || ! collections.users) return next(new Error('No collections.'))
   req.collections = collections;
   return next();
 });
 
-
-
-// all environments
+// Express.js configurations
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.cookieParser('3CCC4ACD-6ED1-4844-9217-82131BDCB239'));
-app.use(express.session({secret: '2C44774A-D649-4D44-9535-46E296EF984F'}))
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+
+// Express.js middleware configuration
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser('3CCC4ACD-6ED1-4844-9217-82131BDCB239'));
+app.use(session({secret: '2C44774A-D649-4D44-9535-46E296EF984F'}))
+app.use(methodOverride());
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Authentication middleware
 app.use(function(req, res, next) {
   if (req.session && req.session.admin)
     res.locals.admin = true;
   next();
 });
 
-//authorization
+// Authorization Middleware
 var authorize = function(req, res, next) {
   if (req.session && req.session.admin)
     return next();
@@ -50,14 +58,11 @@ var authorize = function(req, res, next) {
     return res.send(401);
 };
 
-// development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
-
-app.use(app.router);
-//PAGES&ROUTES
+// PAGES&ROUTES
 app.get('/', routes.index);
 app.get('/login', routes.user.login);
 app.post('/login', routes.user.authenticate);
@@ -67,12 +72,13 @@ app.get('/post', authorize, routes.article.post);
 app.post('/post', authorize, routes.article.postArticle);
 app.get('/articles/:slug', routes.article.show);
 
-//REST API ROUTES
+// REST API ROUTES
 app.all('/api', authorize);
 app.get('/api/articles', routes.article.list)
 app.post('/api/articles', routes.article.add);
 app.put('/api/articles/:id', routes.article.edit);
 app.del('/api/articles/:id', routes.article.del);
+
 
 
 
