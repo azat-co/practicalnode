8289c6dca4b1f615ea5d3f0e8f980350427d42b5
Azat Mardanov <azat.mardanov@gmail.com>
2014-05-21 10:28:44 -0700

    replace quotes

--- ch7/blog-express/routes/user.js
+++ ch7/blog-express/routes/user.js
@@ -4,7 +4,7 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.send('respond with a resource');
 };
 
 
@@ -32,13 +32,13 @@ exports.logout = function(req, res, next) {
 
 exports.authenticate = function(req, res, next) {
   if (!req.body.email || !req.body.password)
-    return res.render('login', {error: "Please enter your email and password."});
+    return res.render('login', {error: 'Please enter your email and password.'});
   req.models.User.findOne({
     email: req.body.email,
     password: req.body.password
   }, function(error, user){
     if (error) return next(error);
-    if (!user) return res.render('login', {error: "Incorrect email&password combination."});
+    if (!user) return res.render('login', {error: 'Incorrect email&password combination.'});
     req.session.user = user;
     req.session.admin = user.admin;
     res.redirect('/admin');
