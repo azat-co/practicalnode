1b627140cbd15b0ff979edd7990ffeaa443588e4
Azat Mardanov <azat.mardanov@gmail.com>
2014-05-20 17:18:04 -0700

    move chapter 4 files into the ch4 folder'

--- ch4/consolidate/app.js
+++ ch4/consolidate/app.js
@@ -2,12 +2,11 @@ var express = require('express'),
   cons = require('consolidate'),
   app = express()
 
-// assign the swig engine to .html files
 app.engine('html', cons.swig)
 
-// set .html as the default extension
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
 
 var platforms = [
   { name: 'node' },
@@ -18,15 +17,15 @@ var platforms = [
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Consolidate This'
-  });
-});
+  })
+})
 
 app.get('/platforms', function(req, res){
   res.render('platforms', {
     title: 'Platforms',
     platforms: platforms
-  });
-});
+  })
+})
 
-app.listen(3000);
-console.log('Express server listening on port 3000');
+app.listen(3000)
+console.log('Express server listening on port 3000')
