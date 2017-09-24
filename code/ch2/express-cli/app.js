afd8897008ce4be5a80fdd4d4370fbd8faf9eb96
Azat Mardan <hi@azat.co>
2017-09-24 15:58:21 +0500

    add changes :cookie:

--- code/ch2/express-cli/app.js
+++ code/ch2/express-cli/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,7 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
