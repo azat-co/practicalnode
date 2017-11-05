02dba91ea8c38c3b92abdb16b61bce53c644a201
Azat Mardan <hi@azat.co>
2017-11-05 16:32:13 -0600

    update to latest mongoskin

--- code/ch5/mongo-skin.js
+++ code/ch5/mongo-skin.js
@@ -1,21 +1,22 @@
 const mongoskin = require('mongoskin')
+const { toObjectID } = mongoskin.helper
 const dbHost = '127.0.0.1'
 const dbPort = 27017
 
-var db = mongoskin.db(`${dbHost}:${dbPort}/local`, {safe: true})
+var db = mongoskin.db(`mongodb://${dbHost}:${dbPort}/local`, {safe: true})
 
-db.bind('messages', {
-  findOneAndAddText: function (text, fn) {
-    db.collection('messages').findOne({}, (error, item) => {
+db.bind('messages').bind({
+  findOneAndAddText: function (text, fn) { // no fat arrow fn because we need to let bind pass the collection to use this on the next line... this can be replaced with db.messages too
+    this.findOne({}, (error, document) => {
       if (error) {
         console.error(error)
         return process.exit(1)
       }
-      console.info('findOne: ', item)
-      item.text = text
-      var id = item._id.toString() // we can store ID in a string
-      console.info('before saving: ', item)
-      db.collection('messages').save(item, (error, count) => {
+      console.info('findOne: ', document)
+      document.text = text
+      var id = document._id.toString() // we can store ID in a string
+      console.info('before saving: ', document)
+      this.save(document, (error, count) => {
         if (error) {
           console.error(error)
           return process.exit(1)
@@ -27,15 +28,15 @@ db.bind('messages', {
   }
 })
 
-db.collection('messages').findOneAndAddText('hi', (count, id) => {
-  db.collection('messages').find({
-    _id: db.collection('messages').id(id)
-  }).toArray((error, items) => {
+db.messages.findOneAndAddText('hi', (count, id) => {
+  db.messages.find({
+    _id: toObjectID(id)
+  }).toArray((error, documents) => {
     if (error) {
       console.error(error)
       return process.exit(1)
     }
-    console.info('find: ', items)
+    console.info('find: ', documents)
     db.close()
     process.exit(0)
   })
