3b435f9c76c2ced1f508726250e6f89bbf03590d
Azat Mardan <hi@azat.co>
2017-11-06 17:20:14 -0600

    add changes :notebook_with_decorative_cover:

--- code/ch7/mongoose-example/mongoose.js
+++ code/ch7/mongoose-example/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test', {useMongoClient: true})
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 const Book = mongoose.model('Book', { name: String })
 
 const practicalNodeBook = new Book({ name: 'Practical Node.js' })
 practicalNodeBook.save(function (err, results) {
   if (err) {
-    console.error(e)
+    console.error(err)
     process.exit(1)
   } else {
     console.log('Saved: ', results)
