0d6f2fc3c88c45ace082fd4f2f71b71f34aeed90
Azat Mardan <hi@azat.co>
2017-11-07 11:26:56 -0600

    make websocket (ws) example more interesting with vanilla js

--- code/ch9/ws-basic/server.js
+++ code/ch9/ws-basic/server.js
@@ -3,6 +3,9 @@ const wss = new WebSocketServer({port: 3000})
 
 wss.on('connection', (ws) => {
   ws.send('XYZ')
+  setInterval(()=>{
+    ws.send((new Date).toLocaleTimeString())
+  }, 1000)
   ws.on('message', (message) => {
     console.log('received: %s', message)
   })
