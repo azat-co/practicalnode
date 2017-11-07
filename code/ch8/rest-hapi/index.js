6e815332f4c1e7cc448e4bf33a8e7a9498f2f41a
Azat Mardan <hi@azat.co>
2017-11-07 10:58:25 -0600

    bring to standard

--- code/ch8/rest-hapi/index.js
+++ code/ch8/rest-hapi/index.js
@@ -1,16 +1,16 @@
 const port = process.env.PORT || 3000
 
-const Hapi = require('hapi');
+const Hapi = require('hapi')
 
-const server = new Hapi.Server();
-server.connection({ port: port, host: 'localhost' });
+const server = new Hapi.Server()
+server.connection({ port: port, host: 'localhost' })
 
 const mongoskin = require('mongoskin')
 
-var db = mongoskin.db('mongodb://@localhost:27017/test', {})
-var id = mongoskin.helper.toObjectID
+const db = mongoskin.db('mongodb://@localhost:27017/test', {})
+const id = mongoskin.helper.toObjectID
 
-var loadCollection = function(name, callback) {
+const loadCollection = (name, callback) => {
   callback(db.collection(name))
 }
 
@@ -18,99 +18,98 @@ server.route([
   {
     method: 'GET',
     path: '/',
-    handler: function(req, reply) {
+    handler: (req, reply) => {
       reply('Select a collection, e.g., /collections/messages')
     }
   },
-    {
+  {
     method: 'GET',
     path: '/collections/{collectionName}',
-    handler: function(req, reply) {
-      loadCollection(req.params.collectionName, function(collection) {
-        collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function(e, results){
+    handler: (req, reply) => {
+      loadCollection(req.params.collectionName, (collection) => {
+        collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function (e, results) {
           if (e) return reply(e)
           reply(results)
         })
       })
     }
   },
-    {
+  {
     method: 'POST',
     path: '/collections/{collectionName}',
-    handler: function(req, reply) {
-      loadCollection(req.params.collectionName, function(collection) {
-        collection.insert(req.payload, {}, function(e, results){
+    handler: (req, reply) => {
+      loadCollection(req.params.collectionName, (collection) => {
+        collection.insert(req.payload, {}, function (e, results) {
           if (e) return reply(e)
           reply(results.ops)
         })
       })
     }
   },
-    {
+  {
     method: 'GET',
     path: '/collections/{collectionName}/{id}',
-    handler: function(req, reply) {
-      loadCollection(req.params.collectionName, function(collection) {
-        collection.findOne({_id: id(req.params.id)}, function(e, result){
+    handler: (req, reply) => {
+      loadCollection(req.params.collectionName, (collection) => {
+        collection.findOne({_id: id(req.params.id)}, (e, result) => {
           if (e) return reply(e)
           reply(result)
         })
       })
     }
   },
-    {
+  {
     method: 'PUT',
     path: '/collections/{collectionName}/{id}',
-    handler: function(req, reply) {
-      loadCollection(req.params.collectionName, function(collection) {
+    handler: (req, reply) => {
+      loadCollection(req.params.collectionName, (collection) => {
         collection.update({_id: id(req.params.id)},
           {$set: req.payload},
-          {safe: true, multi: false}, function(e, result){
-          if (e) return reply(e)
-          reply((result.result.n === 1) ? {msg:'success'} : {msg:'error'})
-        })
+          {safe: true, multi: false}, (e, result) => {
+            if (e) return reply(e)
+            reply((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+          })
       })
     }
   },
-    {
+  {
     method: 'DELETE',
     path: '/collections/{collectionName}/{id}',
-    handler: function(req, reply) {
-      loadCollection(req.params.collectionName, function(collection) {
-        collection.remove({_id: id(req.params.id)}, function(e, result){
-           if (e) return reply(e)
-           reply((result.result.n === 1) ? {msg:'success'} : {msg:'error'})
-         })
+    handler: (req, reply) => {
+      loadCollection(req.params.collectionName, (collection) => {
+        collection.remove({_id: id(req.params.id)}, (e, result) => {
+          if (e) return reply(e)
+          reply((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
       })
     }
   }
 ])
 
-
-var options = {
+const options = {
   subscribers: {
     'console': ['ops', 'request', 'log', 'error']
   }
-};
+}
 
-server.register(require('good', options, function (err) {
+server.register(require('good', options, (err) => {
   if (!err) {
       // Plugin loaded successfully
   }
 }))
 
-
 const boot = () => {
-  server.start((err) => {    
+  server.start((err) => {
     if (err) {
-      process.exit(1)
+      console.error(err)
+      return process.exit(1)
     }
     console.log(`Server running at: ${server.info.uri}`)
-  })    
+  })
 }
 
 const shutdown = () => {
-  server.stop({}, ()=>{
+  server.stop({}, () => {
     process.exit(0)
   })
 }
