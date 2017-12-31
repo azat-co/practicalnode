7570e9368d2e292eb6c27d41a04a31683f5a9489
Azat Mardan <hi@azat.co>
2017-12-31 11:58:40 -0800

    bring to standard

--- code/ch6/jwt-example/server.js
+++ code/ch6/jwt-example/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 const courses = [
   {title: "You Don't Know Node"},
-  {title: "AWS Intro"}
+  {title: 'AWS Intro'}
 ]
 const users = []
 const auth = (req, res, next) => {
@@ -18,41 +18,40 @@ const auth = (req, res, next) => {
       console.log('authenticated as ', decoded.username)
       next()
     })
-  }
-  else return res.status(401).send()
+  } else return res.status(401).send()
 }
 
 app.get('/courses', (req, res) => {
-    res.send(courses)
-  })
+  res.send(courses)
+})
 app.post('/courses', auth, (req, res) => {
-    courses.push({title: req.body.title})
-    res.send(courses)
-  })
+  courses.push({title: req.body.title})
+  res.send(courses)
+})
 
 app.post('/auth/register', (req, res) => {
-    bcrypt.hash(req.body.password, 10, (error, hash)=>{
-      if (error) return res.status(500).send()
-      users.push({
-        username: req.body.username,
-        passwordHash: hash
-      })
-      res.status(201).send('registered')
+  bcrypt.hash(req.body.password, 10, (error, hash) => {
+    if (error) return res.status(500).send()
+    users.push({
+      username: req.body.username,
+      passwordHash: hash
     })
+    res.status(201).send('registered')
   })
+})
 
 app.post('/auth/login', (req, res) => {
-    const foundUser = users.find((value, index, list) => {
-      if (value.username === req.body.username) return true
-      else return false
-    })
-    if (foundUser) {
-      bcrypt.compare(req.body.password, foundUser.passwordHash, (error, matched) => {
-        if (!error && matched) {
-          res.status(201).json({token: jwt.sign({ username: foundUser.username}, SECRET)})
-        } else res.status(401).send()
-      })
-    } else res.status(401).send()
+  const foundUser = users.find((value, index, list) => {
+    if (value.username === req.body.username) return true
+    else return false
   })
+  if (foundUser) {
+    bcrypt.compare(req.body.password, foundUser.passwordHash, (error, matched) => {
+      if (!error && matched) {
+        res.status(201).json({token: jwt.sign({ username: foundUser.username}, SECRET)})
+      } else res.status(401).send()
+    })
+  } else res.status(401).send()
+})
 
 app.listen(3000)
